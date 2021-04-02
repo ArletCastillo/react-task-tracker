f3e2ec101acafda1a32fc17dc5662fff12845538
lebron.a <lebron.a@infox.de>
2021-04-02 11:54:33 -0400

    Toggle add task button done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from "react";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -24,6 +26,13 @@ function App() {
     }
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -31,12 +40,17 @@ function App() {
 
   // Toggle Reminder
   const toggleReminder = (id) => {
-    console.log(id)
+    // the thre dots ... mean that it will propagate the change to all the tasks in the list
+    setTasks(
+      tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    )
   }
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={ () => setShowAddTask(!showAddTask) }
+              showAdd={ showAddTask }/>
+      {showAddTask && <AddTask onAdd={ addTask } />}
       {tasks.length > 0 ? (<Tasks tasks={ tasks } onDelete={ deleteTask } onToggle={ toggleReminder }/>) : ('No tasks to show')}      
     </div>
   );
